b19c4d77d32ff3985d72b821e35521e0eb9da5e7
Annisa6780 <ilar6780@gmail.com>
2021-04-24 14:54:50 +0700

    WSI (M7)

---
+++ Minggu_7/Latihan/Pewarisan/A.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author Annisa Laila
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TambpilkanNilaiXY() {
+        System.out.println("Nilai X : " + x + ", y ; " + y);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Minggu_7/Tugas/Tugas2/A.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas2;
+
+/**
+ *
+ * @author Annisa Laila
+ */
+class A {
+    void callthis(){
+        System.out.println("Inside Class A's Method!");
+    }
+    
+}
